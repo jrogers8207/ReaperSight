0a4b43c8a7fca3a9d10b6fa7b8cf07d994f54a8c
Jack Rogers <jack.rogers@opswat.com>
2023-03-24 13:04:16 -0400

    Initial commit.

---
+++ build.gradle.kts
@@ -0,0 +1,79 @@
+plugins {
+    kotlin("multiplatform") version "1.8.10"
+    id("com.android.application")
+    id("app.cash.sqldelight") version "2.0.0-alpha05"
+}
+
+group = "me.jackrogers"
+version = "1.0"
+
+repositories {
+    google()
+    mavenCentral()
+}
+
+kotlin {
+    jvm {
+        compilations.all {
+            kotlinOptions.jvmTarget = "1.8"
+        }
+        testRuns["test"].executionTask.configure {
+            useJUnitPlatform()
+        }
+    }
+    android()
+    sourceSets {
+        val ktorVersion = "2.2.4"
+        val sqldelightVersion = "2.0.0-alpha05"
+        val commonMain by getting {
+            dependencies {
+                implementation("io.ktor:ktor-client-core:$ktorVersion")
+            }
+        }
+        val commonTest by getting {
+            dependencies {
+                implementation(kotlin("test"))
+            }
+        }
+        val jvmMain by getting {
+            dependencies {
+                implementation("io.ktor:ktor-client-okhttp:$ktorVersion")
+                implementation("app.cash.sqldelight:sqlite-driver:$sqldelightVersion")
+            }
+        }
+        val jvmTest by getting
+        val androidMain by getting {
+            dependencies {
+                implementation("com.google.android.material:material:1.6.1")
+                implementation("io.ktor:ktor-client-okhttp:$ktorVersion")
+                implementation("app.cash.sqldelight:android-driver:$sqldelightVersion")
+            }
+        }
+        val androidTest by getting {
+            dependencies {
+                implementation("junit:junit:4.13.2")
+            }
+        }
+    }
+}
+
+android {
+    compileSdkVersion(32)
+    sourceSets["main"].manifest.srcFile("src/androidMain/AndroidManifest.xml")
+    defaultConfig {
+        applicationId = "me.jackrogers.library"
+        minSdkVersion(24)
+        targetSdkVersion(32)
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+}
+sqldelight {
+    databases {
+        create("Database") {
+            packageName.set("com.example")
+        }
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        jcenter()
+        gradlePluginPortal()
+        mavenCentral()
+    }
+    resolutionStrategy {
+        eachPlugin {
+            if (requested.id.namespace == "com.android") {
+                useModule("com.android.tools.build:gradle:7.2.2")
+            }
+        }
+    }
+}
+rootProject.name = "ReaperSight"
+
